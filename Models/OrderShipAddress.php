2d3e207536b55d038b96a0c0b81e976d6c0c8e29
sayandipwebart <sayandip.saha@webart.technology>
2023-12-01 15:22:26 +0530

    First time commiting for the first stage of ecommerce modules

---
+++ Models/OrderShipAddress.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderShipAddress extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'order_id',
+        'f_name',
+        'l_name',
+        'company_name',
+        'country',
+        'street',
+        'city',
+        'district',
+        'zip',
+        'mobile',
+        'email',
+    ];
+}
